bd963c9278b595f973e9cc0e21688fdaabdaf020
artnest <artnest789@gmail.com>
2019-10-03 19:49:50 +0300

    Move the camera in LateUpdate callback instead of Update to guarantee the correct position

--- Unity/Roll-a-Ball/Assets/Scripts/CameraController.cs
+++ Unity/Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -15,7 +15,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
